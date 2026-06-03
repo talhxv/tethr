a5ab402ec9736f8aaa3d1c19621fbc6e32e35f55
talhxv <txlhxv@gmail.com>
2026-06-04 02:26:34 +0500

    Revert "Add services section with Software, Automations, Consulting cards"
    This reverts commit d236cc2b6cb112b5598aecd2f45f4a66ed3cbc53.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './style.css'
 import { initGridAlive } from './grid-alive.js'
 import { html as navbarHtml, init as initNavbar } from './components/navbar.js'
 import { html as heroHtml, init as initHero } from './components/hero.js'
-import { html as servicesHtml } from './components/services.js'
 import { html as manifestoHtml } from './components/manifesto.js'
 import { html as hiringHtml, init as initHiring } from './components/hiring.js'
 import { html as ctaHtml } from './components/cta.js'
@@ -11,7 +10,6 @@ import { html as footerHtml, init as initFooter } from './components/footer.js'
 document.querySelector('#app').innerHTML = `
 <div class="navbar-wrap">${navbarHtml}</div>
 ${heroHtml}
-${servicesHtml}
 ${manifestoHtml}
 ${hiringHtml}
 ${ctaHtml}
